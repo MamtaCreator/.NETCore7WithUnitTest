fa0150dbae990d01357974b0fb7741bae2d0aef8
CitiustechGithub <113910997+CitiustechGithub@users.noreply.github.com>
2023-04-14 19:30:48 +0530

    Added Class Library model fields

---
+++ NETCore_7/LoginMiddleware/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    /// <summary>
+    /// Domain model for country
+    /// </summary>
+    public class Country
+    {
+        public Guid CountryId { get; set; }
+        public string? CountryName { get; set; }
+
+    }
+}
---
+++ NETCore_7/LoginMiddleware/XunitPracProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace XunitPracProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
