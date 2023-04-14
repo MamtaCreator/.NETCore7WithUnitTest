1253f3b92c55ffc9d089998127debff49fca2a84
CitiustechGithub <113910997+CitiustechGithub@users.noreply.github.com>
2023-04-14 19:32:10 +0530

    new changes

--- NETCore_7/LoginMiddleware/ServiceContract/DTO/CountryResponse.cs
+++ NETCore_7/LoginMiddleware/ServiceContract/DTO/CountryResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace ServiceContract.DTO
 {
-    internal class CountryResponse
+
+    /// <summary>
+    /// DTO method that is used as a return type for most of the countryservices methods 
+    /// </summary>
+    public class CountryResponse
     {
+        public Guid CountryId { get; set; }
+        public string? CountryName { get; set; }
+    }
+    public static class CountryExtension
+    {
+        public static CountryResponse ToCountryResponse(this Country country)
+        {
+            return new CountryResponse 
+            {
+              CountryId= country.CountryId,
+              CountryName= country.CountryName,
+            };
+        }
     }
 }
