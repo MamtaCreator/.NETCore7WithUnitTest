947dec017b0908d47268ae4926e8784d9637dd6b
CitiustechGithub <113910997+CitiustechGithub@users.noreply.github.com>
2023-04-14 16:13:22 +0530

    LoginMiddleware code been added

---
+++ NETCore_7/LoginMiddleware/LoginMiddleware/CustomLoginMiddleware/LoginMiddleware.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace LoginMiddleware.CustomLoginMiddleware
+{
+    public class LoginMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoginMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Path == "/" && context.Request.Method == "POST") 
+            {
+                StreamReader reader = new StreamReader(context.Request.Body);
+                string body  = await reader.ReadToEndAsync();
+
+
+                Dictionary<string, StringValues> queryDict = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+
+                string?email = null,password=null;
+                if(queryDict.ContainsKey("email")) 
+                { 
+                   email = Convert.ToString(queryDict["email"][0]);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid input for email'\n'");
+                }
+                if(queryDict.ContainsKey("password")) 
+                { 
+                  password = Convert.ToString(queryDict["password"][0]);
+                }
+                else
+                {
+                    if (context.Response.StatusCode == 200)
+                        context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid input for password '\n'");
+                }
+                if(string.IsNullOrEmpty(email)== false && string.IsNullOrEmpty(password)==false)
+                {
+                    string validEmail = "admin@example.com", validPassword = "admin1234";
+                     bool isValidLogin = false;
+
+                    if(email == validEmail && password == validPassword) 
+                    {
+                        isValidLogin= true;
+                    }
+                    else
+                    {
+                        isValidLogin= false;
+                    }
+                    if(isValidLogin)
+                    {
+                        await context.Response.WriteAsync("Login successfull!'\n'");
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                        await context.Response.WriteAsync("Invalid login '\n'");
+                    }
+                }
+            
+            
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+    public static class LoginMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoginMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<LoginMiddleware>();
+        }
+    }
+
+}
--- NETCore_7/LoginMiddleware/xUnitPract/Program.cs
+++ NETCore_7/LoginMiddleware/xUnitPract/Program.cs
@@ -1,9 +1,5 @@
-using LoginMiddleware.CustomLoginMiddleware;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.UseLoginMiddleware();
 
 app.MapGet("/", () => "Hello World!");
 
